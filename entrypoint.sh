aecd8116c9b99ab408272b24b4b0c4320b7157b9
Matt Lord <mattalord@gmail.com>
2019-08-11 11:59:22 -0400

    Add stunnel support
    This very useful for connecting to Cloud Redis services
    (e.g. ElastiCache) that have TLS enabled.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,23 @@ set -e
 
 LOCALPORT=$1
 REMOTEHOSTPORT=$2
+SSL=$3
+PROTOCOL="tcp"
 
 if [[ -z "$LOCALPORT" || -z "$REMOTEHOSTPORT" ]]; then
-    echo "You must specify two parameters: <localport> <remotehost:port>"
+    echo "You must specify two parameters: <localport> <remotehost:port> [stunnel]"
     exit 1
 fi
 
-echo "Setting up port forwarding for localhost:$1 -> $2"
+echo -n "Setting up port forwarding for localhost:$1 -> $2"
 
-CMD="socat tcp-listen:$LOCALPORT,reuseaddr,fork tcp:$REMOTEHOSTPORT"
+if [[ ! -z "$SSL" && "$SSL" == "stunnel" ]]; then
+    echo " using an SSL tunnel"
+    PROTOCOL="openssl-connect"
+else
+    echo 
+fi
+
+CMD="socat tcp-listen:$LOCALPORT,reuseaddr,fork $PROTOCOL:$REMOTEHOSTPORT"
 
 exec $CMD
