27867925c4261f48945e60aec67278402e32d8e4
Matt Lord <mattalord@gmail.com>
2021-04-23 21:22:41 -0400

    Be pedantic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
+set -euo pipefail
 
-set -e 
-
-LOCALPORT=$1
-REMOTEHOSTPORT=$2
-SSL=$3
+LOCALPORT=${1:-""}
+REMOTEHOSTPORT=${2:-""}
+SSL=${3:-""}
 PROTOCOL="tcp"
 
-if [[ -z "$LOCALPORT" || -z "$REMOTEHOSTPORT" ]]; then
+if [[ -z ${LOCALPORT} || -z ${REMOTEHOSTPORT} ]]; then
     echo "You must specify two parameters: <localport> <remotehost:port> [stunnel]"
     exit 1
 fi
 
-echo -n "Setting up port forwarding for localhost:$1 -> $2"
+echo -n "Setting up port forwarding for localhost:${LOCALPORT} -> ${REMOTEHOSTPORT}"
 
-if [[ ! -z "$SSL" && "$SSL" == "stunnel" ]]; then
+if [[ ${SSL} == "stunnel" ]]; then
     echo " using an SSL tunnel"
     PROTOCOL="openssl-connect"
 else
-    echo 
+    echo
 fi
 
-CMD="socat tcp-listen:$LOCALPORT,reuseaddr,fork $PROTOCOL:$REMOTEHOSTPORT"
+CMD="socat tcp-listen:$LOCALPORT,reuseaddr,fork ${PROTOCOL}:${REMOTEHOSTPORT}"
 
-exec $CMD
+exec "${CMD}"
